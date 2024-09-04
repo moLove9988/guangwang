6d7724863860fffaf0941440cab2518eaf26bd04
LAPTOP-J21MTLH6\tfkj0 <morelacolver@gmail.com>
2024-09-04 09:37:15 +0800

    添加内容

---
+++ src/pages/app-ads.txt.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts" generic="T extends any, O extends any">
+defineOptions({
+  name: 'IndexPage',
+})
+let contents = `google.com, pub-5069848297396952, DIRECT, f08c47fec0942fa0`</script>
+
+<template>
+  <div class="protocol" v-html="contents">
+
+  </div>
+</template>
+
+<style scoped>
+  .protocol {
+    font-size: 14px;
+    margin-bottom: 14px;
+    color: #000;
+    margin-left:6px;
+    margin-right:6px;
+    background-color: fff;
+    
+  }
+  .bt {
+    font-size: 20px;
+    margin: 6px 0px;
+    color: #7f1146;
+  }
+</style>
