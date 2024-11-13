817972636f6cb27b682d54a0a864af473f43e0c3
LAPTOP-J21MTLH6\tfkj0 <morelacolver@gmail.com>
2024-11-13 15:27:56 +0800

    VintAre

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,7 +10,7 @@ defineOptions({
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
       <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        mifPlayjnhhs@gail.com
+        vintAre33451008@gmail.com
       </div>
     </div>
   </div>
