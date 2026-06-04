27a497f8fe035c1300444fd451a4fcbc082c491a
zhangsan <zhangsan666@163.com>
2026-06-04 11:49:14 +0800

    666

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,7 +10,7 @@ defineOptions({
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
       <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        Brubo99826@gmail.com
+        Recio99826@gmail.com
       </div>
     </div>
   </div>
--- src/pages/privacy.vue
+++ src/pages/privacy.vue
@@ -2,7 +2,7 @@
 defineOptions({
   name: 'IndexPage',
 })
-const appName = 'Brubo'
+const appName = 'Recio'
 </script>
 
 <template>
