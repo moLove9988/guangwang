6eddf18e433b0c5abfa1c46252e73a011ccb1c95
zhangsan <zhangsan666@163.com>
2026-07-30 10:27:52 +0800

    666

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,6 +6,19 @@ defineOptions({
 
 <template>
   <div relative min-h-100vh>
-    
+    <img
+      src="http://www.pumqewy2345.xyz/hfimg/img/e78e0ed5149f42d695c7146cc4cdaa52584.png"
+      alt="Kosvi"
+      class="page-image"
+    >
   </div>
 </template>
+
+<style scoped>
+.page-image {
+  display: block;
+  width: 100%;
+  max-width: 100%;
+  height: auto;
+}
+</style>
