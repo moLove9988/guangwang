3892471b94d357a222f382b0d87625c555437269
comuser <comuser>
2026-08-19 10:04:13 +0800

    22

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts" generic="T extends any, O extends any">
-defineOptions({
-  name: 'IndexPage',
-})
+defineOptions({ name: 'IndexPage' })
 </script>
 
 <template>
-  <div relative min-h-100vh>
-    <iframe src="/my.html" width="100%" height="100%" frameborder="0"></iframe>
+  <!-- 滚动条遮罩容器，裁剪右侧滚动条 -->
+  <div class="iframe-container">
+    <iframe src="/my.html" class="content-iframe"></iframe>
   </div>
 </template>
+
+<style scoped>
+.iframe-container {
+  position: fixed;
+  inset: 0;
+  width: calc(100vw + 20px); /* 向右拓宽，把滚动条推出可视区域 */
+  height: 100vh;
+  overflow: hidden;
+}
+
+.content-iframe {
+  width: 100%;
+  height: 100%;
+  border: none;
+}
+</style>
